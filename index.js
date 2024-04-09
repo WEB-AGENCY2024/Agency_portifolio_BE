1734805a32320bf14bdeac807dd0a59a75ffe95e
Louisseverin <113350620+Louisseverin@users.noreply.github.com>
2024-04-08 21:45:26 -0400

    Setting the Database MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const Welcome = require("./routes/welcome");
 
 const app = express();
-dotenv.config();
+dotenv.config({ path: "./config.env" });
 //import the port
 const port = process.env.PORT;
 //todo :add the database here
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful"));
 
 //SWAGGER TO TEST OUR API
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
